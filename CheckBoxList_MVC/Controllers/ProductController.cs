3a07c6a124b5e8e182d62b07ea33419b48871dcb
Ertugrul <57365047+officialerto@users.noreply.github.com>
2022-12-25 11:11:06 +0300

    Add project files.

---
+++ CheckBoxList_MVC/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CheckBoxList_MVC.Models;
+
+namespace CheckBoxList_MVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            ProductModel product = new ProductModel();
+            using (DBModels db=new DBModels())
+            {
+                product.Products = db.Products.ToList<Product>();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Index(ProductModel model)
+        {
+            var selectedProduct = model.Products.Where(x => x.IsChecked == true).ToList<Product>();
+
+            return Content(String.Join("-",selectedProduct.Select(x=>x.ProductName)));
+        }
+
+    }
+}
